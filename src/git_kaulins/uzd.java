e5b877cc3e350cb1fef1b52731f9ad9eb841c395
katlala <katrina.skudrite@sk.lvt.lv>
2025-09-02 12:15:37 +0300

    Ieviesta iespēja ievadīt metienu skaitu.

--- src/git_kaulins/uzd.java
+++ src/git_kaulins/uzd.java
@@ -1,15 +1,22 @@
 package git_kaulins;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class uzd {
 
 	public static void main(String[] args) {
-		int sk;
+		int sk, reizes;
 		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kauliņu?");
+		reizes = scan.nextInt();
+		for (int i=1; i<reizes; i++) {
 		sk = rand.nextInt(6)+1; // ieskseejaa pieskaita par +1
 		System.out.println("Uzkrita skaitlis: " + sk);
-
+		}
+		
+		scan.close();
 	}
 
 }
