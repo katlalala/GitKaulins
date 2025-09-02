feb4ad9fe67e7c08843d40474e50d56b08bba1a6
katlala <katrina.skudrite@sk.lvt.lv>
2025-09-02 11:42:08 +0300

    Skaitļa uzmešana tiek realizēta.

--- src/git_kaulins/uzd.java
+++ src/git_kaulins/uzd.java
@@ -1,9 +1,14 @@
 package git_kaulins;
 
+import java.util.Random;
+
 public class uzd {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int sk;
+		Random rand = new Random();
+		sk = rand.nextInt(6)+1; // ieskseejaa pieskaita par +1
+		System.out.println("Uzkrita skaitlis: " + sk);
 
 	}
 
