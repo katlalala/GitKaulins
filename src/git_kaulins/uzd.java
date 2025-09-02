8ff1f2ed6873f78908178ff21e778acb2fcbd759
katlala <katrina.skudrite@sk.lvt.lv>
2025-09-02 12:02:30 +0300

    Izveidota metode kauliņa mešanai.

--- src/git_kaulins/uzd.java
+++ src/git_kaulins/uzd.java
@@ -3,13 +3,16 @@ package git_kaulins;
 import java.util.Random;
 
 public class uzd {
-
-	public static void main(String[] args) {
-		int sk;
+	static void mestKaulinu() {
+		
 		Random rand = new Random();
-		sk = rand.nextInt(6)+1; // ieskseejaa pieskaita par +1
+		int sk = rand.nextInt(6)+1; // ieskseejaa pieskaita par +1
 		System.out.println("Uzkrita skaitlis: " + sk);
 
+	}
+
+	public static void main(String[] args) {
+		mestKaulinu();
 	}
 
 }
